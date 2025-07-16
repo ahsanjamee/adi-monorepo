8bb6973c6ca5e78e31ced3b17bd3015388ae2184
nsiddiquey <nsiddiquey@gmail.com>
2025-07-16 16:44:25 +0600

    update to add prescription

--- apps/store/src/layout/navbar/NavbarPromo.js
+++ apps/store/src/layout/navbar/NavbarPromo.js
@@ -376,6 +376,14 @@ const NavbarPromo = () => {
                 >
                 {'Registration'}
               </Link>
+
+              <Link
+                target="_blank"
+                href="https://portal.adibd.net/auth/login"
+                className="relative inline-flex items-center  bg-green-700 font-serif ml-4 py-2 px-4 rounded text-sm font-semibold text-white hover:bg-green-600"
+                >
+                {'Prescription1'}
+              </Link>
             
           </div>
         </div>
